9d4cc9bbdc071d53ec699d45baee4dbec07942f7
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-01-20 16:14:27 -0500

    Now uses native comment function

--- StringToComment.py
+++ StringToComment.py
@@ -1,29 +1,19 @@
 import sublime, sublime_plugin
 
-def _stringToComment(str):
-    words = str.split(" ")
-    total, ret, comment_char = 0, [], '# '
-    max = 80
+def string_to_comment(self, sel, edit):
+    words = self.view.substr(sel).split(" ")
+    total, ret, max = 0, [], 80
 
-    for idx, c in enumerate(words):
-        if total == 0:
-            ret.append(comment_char)
-        if total + len(c) > max:
-            ret.append('\n'+comment_char)
+    for word in words:
+        if total + len(word) > max:
+            ret.append('\n')
             total = 0
-        total += len(c)+1
-        ret.append(c+' ')
-    return "".join(ret)
+        total += len(word)+1
+        ret.append(word+' ')
+    self.view.replace(edit, sel, "".join(ret))
+    self.view.run_command("toggle_comment", {"block" : "false"})
 
 class StringToCommentCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        max, comment_char = 80, '# '
-        new_sels = []
-        sels = self.view.sel()  
-        for sel in sels:  
-            new_sels.append(_stringToComment(self.view.substr(sel)))
-        begin_point = sels[0].begin()
-        for sel in new_sels:
-            self.view.erase(edit, sels[0])
-            self.view.insert(edit, begin_point, sel)
-
+        for sel in self.view.sel():  
+            string_to_comment(self, sel, edit)
