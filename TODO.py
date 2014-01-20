62cf7136b17efde14c60ccc4f2825db14f66e98e
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-01-20 10:33:16 -0500

    Working script

--- TODO.py
+++ TODO.py
@@ -1,32 +1,24 @@
-# Initial scratchpad/thinking.... provbably shouldn't commit this, but hey... :)
-
 sent = "So basically, we have imported our LoginForm class, instantiated an object from it, and sent it down to the template. This is all that is required to get form fields rendered."
 max = 80
-comment_char = '#'
+comment_char = '# '
 
-chars = [x for x in sent]
 chars_delim = sent.split(" ")
-space_bank = []
-ret = []
-total = 0
-
-class sentence(object):
-    def __init__(word=None):
-        if word is None:
-            self.sentence = []
-        else:
-            self.sentence = [word]
-     
-    def append(self, word):
-        self.sentence.append(word + " ")
-
+total, ret = 0, []
 
 for idx, c in enumerate(chars_delim):
-    pass
+    if total == 0:
+        ret.append(comment_char)
+    if total + len(c) > max:
+        ret.append('\n'+comment_char)
+        total = 0
+    total += len(c)+1
+    ret.append(c+' ')
 
-for idx, c in enumerate(chars):
-    if c == ' ':
-        if idx > 80 % 2:
-            ret.append(chars[:[space_bank[-1]]])
+print("".join(ret))
 
-        space_bank.append(idx)
+
+# Output:
+
+# So basically, we have imported our LoginForm class, instantiated an object from
+# it, and sent it down to the template. This is all that is required to get form
+# fields rendered.
