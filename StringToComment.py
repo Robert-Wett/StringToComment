c7be2a3472588c7c4c30756ff9f2832c32341799
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-01-20 22:14:32 -0500

    Update StringToComment.py

--- StringToComment.py
+++ StringToComment.py
@@ -1,40 +1,35 @@
 import sublime, sublime_plugin
 
 
-def string_to_comment(self, sel, edit):
+def str_to_comment(self, sel, edit, add_char=False, char='# '):
+    comment_char = char if add_char else ''
     words = self.view.substr(sel).split(" ")
-    total, ret, max = 0, [], 80
+    comments = format_string(words, d=comment_char)
+    self.view.replace(edit, sel, comments)
+    if not add_char:
+        self.view.run_command("toggle_comment", {"block" : "false"})
 
+def format_string(words, d, max=80):
+    total, str_builder = 0, [d]
     for word in words:
         if total + len(word) > max:
-            ret.append('\n')
+            str_builder.append('\n' + d)
             total = 0
-        total += len(word)+1
-        ret.append(word+' ')
-    self.view.replace(edit, sel, "".join(ret))
-    self.view.run_command("toggle_comment", {"block" : "false"})
-
-
-def default_string_to_comment(self, sel, edit):
-    words = self.view.substr(sel).split(" ")
-    total, ret, max = 0, [], 80
-    default_c = '# '
-    for word in words:
-        if total == 0:
-            ret.append('# ')
-        if total + len(word) > max:
-            ret.append('\n# ')
-            total = 0
-        total += len(word)+1
-        ret.append(word+' ')
-    self.view.replace(edit, sel, "".join(ret))
+        total += len(word) + 1
+        str_builder.append(word + ' ')    
+    return "".join(str_builder)
 
 
 class StringToCommentCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if self.view.settings().get('syntax') == 'Packages/Text/Plain text.tmLanguage':
-            for sel in self.view.sel():
-                default_string_to_comment(self, sel, edit)
-        else: 
-            for sel in self.view.sel():  
-                string_to_comment(self, sel, edit)
+        syntax = self.view.settings().get('syntax')
+        print(syntax)
+        # Individual checks based on languages that don't correctly add comments
+        # when calling the `toggle_comment` command.
+        if 'Plain text' in syntax:
+            [str_to_comment(self, sel, edit, add_char=True) for sel in self.view.sel()]
+        elif 'C#' in syntax or 'ActionScript' in syntax:
+            [str_to_comment(self, sel, edit, add_char=True, char=r'// ')\
+             for sel in self.view.sel()]
+        else:
+            [str_to_comment(self, sel, edit) for sel in self.view.sel()]
